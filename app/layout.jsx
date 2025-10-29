3c3fd2c2ead38c5b7f52d71d448804682932fac5
Yunoya <sonrasamon15725@gmail.com>
2025-10-30 01:34:10 +0700

    add footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Kanit } from 'next/font/google';
 import Navbar from "../components/common/Navbar";
-import Image from "next/image";
+import Footer from "../components/common/Footer";
 
 const kanit = Kanit({
   subsets: ['latin'],
@@ -9,6 +9,7 @@ const kanit = Kanit({
   style: ['normal'],
   display: 'swap',
 });
+
 const logo = "https://queuequres3.s3.us-east-1.amazonaws.com/public/QueueCure_logo+(2).png"
 
 export const metadata = {
@@ -24,11 +25,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={kanit.className}>
-        <main>
-          <Navbar />
+      <body className={`${kanit.className} flex flex-col min-h-screen`}>
+        <Navbar />
+        <main className="flex-grow">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
---
+++ components/common/footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="w-full bg-gray-100 border-t border-gray-200 mt-12 py-6 text-center">
+      <p className="text-sm text-gray-500">
+         💊 QueueCure. Cloud Computing Project 1/2025 (2025).
+      </p>
+    </footer>
+  );
+}
